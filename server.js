cb7efedc2fde3759f79ce487162f3f299f9daeaf
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-12-06 14:41:37 -0800

    added partials for header and footer

--- server.js
+++ server.js
@@ -11,5 +11,11 @@ app.get('/', (req,res) => {
   res.render('index')
 })
 
+app.get('/card', (req,res) => {
+  res.locals.prompt = 'Who is buried in Grants Tomb';
+  res.locals.hint = 'Think about the tomb?';
+  res.render('card')
+});
+
 app.listen(3000, console.log(`Listening on Port: 3000`));
 
