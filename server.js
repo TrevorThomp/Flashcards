54c57142b3dec1b9fa0fada645c05dc171416784
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-12-10 20:52:43 -0800

    modularized routes

--- server.js
+++ server.js
@@ -14,31 +14,6 @@ app.use(cookieParser())
 
 app.set('view engine', 'pug');
 
-app.get('/', (req,res) => {
-  const name = req.cookies.username;
-  name ? res.render('index', { name }) : res.redirect('hello')
-})
-
-app.get('/hello', (req,res) => {
-  res.render('hello')
-})
-
-app.post('/goodbye', (req,res) => {
-  res.clearCookie('username')
-  res.redirect('hello')
-})
-
-app.post('/hello', (req,res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/')
-})
-
-app.get('/card', (req,res) => {
-  res.locals.prompt = 'Who is buried in Grants Tomb';
-  res.locals.hint = 'Think about the tomb?';
-  res.render('card')
-});
-
 // Error Handler
 app.use((req,res,next) => {
   const err = new Error('Not Found');
